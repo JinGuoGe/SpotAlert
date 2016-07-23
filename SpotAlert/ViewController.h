7998af8e8db3edb937e6367c61d008e438754a1a
LionKing <LionKing@lionkings-mac.local>
2016-07-24 03:18:27 +0900

    Initial Commit

---
+++ SpotAlert/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SpotAlert
+//
+//  Created by LionKing on 7/24/16.
+//  Copyright © 2016 bigcity. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
